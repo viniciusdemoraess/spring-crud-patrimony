60bdc8b9fac9a1742eb4aae27a74851cd37ec35b
viniciusdemoraess <demoraesvini@outlook.com>
2022-02-10 13:43:39 -0400

    initial commit

---
+++ src/main/java/com/sesp/patrimonio/app/domain/Patrimony.java
@@ -0,0 +1,15 @@
+package com.sesp.patrimonio.app.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+public class Patrimony {
+
+
+    private String item;
+
+    private String descricao;
+    private String patrimonio;
+    private String situacao;
+    private String valor;
+}
